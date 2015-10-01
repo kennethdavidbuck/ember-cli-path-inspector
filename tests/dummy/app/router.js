19fadda2af5325d4963f15bd6df643c875a06150
Kenneth Buck <kenneth.buck@cibc.com>
2015-10-01 16:39:58 -0400

    added additional routes for visiualization

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,8 +7,20 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
 	this.route('inspect', function () {
-		this.route('one');
-		this.route('two');
-		this.route('three');
+		this.route('one', function () {
+			this.route('one');
+			this.route('two');
+			this.route('three');
+		});
+		this.route('two', function () {
+			this.route('one');
+			this.route('two');
+			this.route('three');
+		});
+		this.route('three', function () {
+			this.route('one');
+			this.route('two');
+			this.route('three');
+		});
 	});
 });
