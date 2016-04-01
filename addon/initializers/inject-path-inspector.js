3be534b9922fabafbdb7dec6d7c32eeedfbb87c4
Kenneth Buck <kenneth@nestedsphere.com>
2016-03-31 20:03:17 -0700

    change inject-path-inspector initializer use new param count, as well as be compatible with previous param counts

--- addon/initializers/inject-path-inspector.js
+++ addon/initializers/inject-path-inspector.js
@@ -1,4 +1,6 @@
-export function initialize(container, application) {
+export function initialize() {
+  const application = arguments[1] || arguments[0];
+
   application.inject('service:path-inspector', 'router', 'router:main');
   application.inject('route', 'pathInspectorService', 'service:path-inspector');
   application.inject('controller', 'pathInspectorService', 'service:path-inspector');
