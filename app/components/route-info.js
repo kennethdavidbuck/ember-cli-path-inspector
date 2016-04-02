584b5b277db54db9b63a15f9244feaa474527cdc
Kenneth Buck <kenneth@nestedsphere.com>
2016-04-02 19:48:48 -0400

    basic pretification and rework of view

---
+++ app/components/route-info.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-path-inspector/components/route-info';
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,20 +1,30 @@
 import Ember from 'ember';
 
+const errorOrLoading = /(loading|error)/;
+
 export default Ember.Controller.extend({
 
-	routeName: 'application',
+  routes: Ember.computed('pathInspectorService.routes.length', function () {
+    const pathInspector = this.get('pathInspectorService');
 
-	node: Ember.computed('routeName', function () {
-		return this.get('pathInspectorService').nodeForRouteName(this.get('routeName'));
-	}),
+    return pathInspector.get('routes')
+      .filter(routeName => !routeName.match(errorOrLoading))
+      .map(routeName => {
+        const node = pathInspector.nodeForRouteName(routeName);
+        const siblingPaths = pathInspector.siblingPathsForRouteName(routeName).filter(routeName => !routeName.match(errorOrLoading));
+        const isLeafRoute = pathInspector.isLeafRouteName(node.routeName);
+        const children =  node.children.filter(childNode => !childNode.routeName.match(errorOrLoading));
 
-	siblingPaths: Ember.computed('routeName', function () {
-		return this.get('pathInspectorService').siblingPathsForRouteName(this.get('routeName'));
-	}),
-
-	actions: {
-		inspect(routeName) {
-			this.set('routeName', routeName);
-		}
-	}
+        return {
+          routeName: node.routeName,
+          pathSegment: node.nodeName,
+          parentPath: node.parent ? node.parent.routeName : 'n/a',
+          childCount: children.length,
+          children: children,
+          siblingCount: siblingPaths.length,
+          siblingPaths: siblingPaths.filter(routeName => !routeName.match(errorOrLoading)),
+          isLeaf: isLeafRoute
+        };
+    });
+  })
 });
