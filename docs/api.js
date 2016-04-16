1baa069c8d0d0951c8d0622aadcadbbc1ded3f2f
Kenneth Buck <kenneth@nestedsphere.com>
2016-04-16 18:36:42 -0400

    version docs for usage in github.io example site

---
+++ docs/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EmberCliRoutePathInspector.Services.PathInspector"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
